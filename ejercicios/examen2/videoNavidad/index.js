3b9122c678b87ae9d8459f8457ecf1b44d01316b
miguifer <miguifer03@gmail.com>
2024-11-12 18:39:53 +0100

    7

---
+++ ejercicios/examen2/videoNavidad/index.js
@@ -0,0 +1,30 @@
+//get the video
+var video = document.getElementById("myVideo");
+//getthebuttom
+var btn = document.getElementById("myBtn");
+
+//get button sound
+var soundBtn = document.getElementById("soundBtn");
+var soundImg = document.getElementById("soundImg");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+function soundMute() {
+  if (video.muted) {
+    video.muted = false;
+    soundImg.src = "sound.png";
+    soundImg.alt = "Sound on";
+  } else {
+    video.muted = true;
+    soundImg.src = "mute.png";
+    soundImg.alt = "Sound off";
+  }
+}

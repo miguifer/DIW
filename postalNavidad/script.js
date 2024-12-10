7b69adf6e7f0b5eb12623a51ede1ae8bac2d7565
miguifer <miguifer03@gmail.com>
2024-12-10 18:40:35 +0100

    papi

--- postalNavidad/script.js
+++ postalNavidad/script.js
@@ -1,4 +1,19 @@
-function iniciarJuego() {
-    let telon =  document.querySelector('.telon');
-    telon.classList.add('telonIdo');
+const objetos = ["baston", "bolaNavidad"];
+
+function iniciarJuego(botIniciar) {
+  let telon = document.querySelector(".telon");
+  telon.classList.add("telonIdo");
+
+  if ((botIniciar.textContent = "Iniciar Juego")) {
+    botIniciar.textContent = "Reinciar Juego";
+  }
+}
+
+let aciertos = 0;
+let errores = 0;
+
+function baston(baston) {
+
+    baston.classList.add('seleccionado');
+
 }

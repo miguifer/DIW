a5e0030825e4b128a57e8435c2fdff9690458e0a
miguifer <miguifer03@gmail.com>
2024-12-10 19:51:32 +0100

    pipiñas

--- postalNavidad/script.js
+++ postalNavidad/script.js
@@ -1,4 +1,4 @@
-const objetos = ["baston", "bolaNavidad"];
+const objetos = ["baston", "bolaNavidad","calcetin"];
 
 let aciertos = 0;
 let errores = 0;
@@ -7,7 +7,7 @@ function iniciarJuego(botIniciar) {
   let telon = document.querySelector(".telon");
   telon.classList.add("telonIdo");
 
-  if ((botIniciar.textContent = "Iniciar Juego")) {
+  if (botIniciar.textContent = "Iniciar Juego") {
     botIniciar.textContent = "Reinciar Juego";
   }
 }
@@ -16,6 +16,11 @@ let objeto = document.querySelector(".objeto");
 objeto.textContent = objetos[0];
 
 function fotoClick(foto) {
+    let baston = document.querySelector(".baston");
+    let bolaNavidad = document.querySelector(".bolaNavidad");
+
+
+
   let objeto = document.querySelector(".objeto");
 
   if (objeto.textContent == "baston" && foto.name == 'baston') {
@@ -23,5 +28,8 @@ function fotoClick(foto) {
     objeto.textContent = objetos[1];
   } else if (objeto.textContent == "bolaNavidad" && foto.name == 'bolaNavidad') {
     bolaNavidad.classList.add("seleccionado");
+    objeto.textContent = objetos[2];
+  }else if (objeto.textContent == "calcetin" && foto.name == 'calcetin') {
+    calcetin.classList.add("seleccionado");
   }
 }
